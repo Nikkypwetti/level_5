537121c21ed9a2108b379bd954016fd5d8780d04
Ganiyu Basirat Olanike <nikkibrownie217@gmail.com>
2023-11-28 09:45:44 +0100

    second  commit

--- src/components/ClassComponent.vue
+++ src/components/ClassComponent.vue
@@ -122,14 +122,14 @@
   </script>
   
   <style>
-  <!-- #app { 
+  #app { 
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  } -->
+  }
   .availableItems {
     background-color: blue;
     padding: 8px 10px;
